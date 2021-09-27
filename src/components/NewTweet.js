896b3d22394e2e5a242524a92aa762b106001153
Evan Parker <65721884+evancp87@users.noreply.github.com>
2021-09-27 21:50:16 +0100

    first commit

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
 
  class NewTweet extends Component {
+     state = {
+         text: '',
+     }
+     handleChange = (e) => {
+         const text =  e.target.value
+
+         this.setstate(() => ({
+             text
+
+         }))
+     } 
+     handleSubmit = (e) => {
+         e.preventDefault()
+        
+        const {text} = this.state
+
+        // TODO: add tweet to the store
+
+        this.setState(() => ({
+            text: '',
+        }))
+        }
     render() {
         return (
             <div>
