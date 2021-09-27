8f472db27fa0f3a79f209a28a0157f8d3364fc07
Evan Parker <65721884+evancp87@users.noreply.github.com>
2021-09-27 21:31:45 +0100

    first commit

---
+++ src/components/NewTweet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+ class NewTweet extends Component {
+    render() {
+        return (
+            <div>
+                New Tweet
+                
+            </div>
+        )
+    }
+}
+
+export default NewTweet
